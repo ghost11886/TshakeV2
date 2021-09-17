4789f8e059ce1889073f4b3e45efa98fe03d2738
ghost11886 <mumu2111999@yahoo.com>
2021-09-17 12:38:29 +0300

    Update tg.py

--- utlis/tg.py
+++ utlis/tg.py
@@ -71,8 +71,10 @@ def Ckuser(message):
 
   userID = message.from_user.id
   chatID = message.chat.id
-  response = requests.get('https://api.telegram.org/bot{}/getchatmember?chat_id=@im_kich"&user_id={}'.format(TOKEN,userID)).json()
-  if response["status"] == "left" or "kicked":
-    return False
-  else:
+  response = requests.get('https://api.telegram.org/bot{}/getchatmember?chat_id=@im_kich&user_id={}'.format(TOKEN,userID)).json()
+  if response['result']['status'] == 'creator' or 'member' or 'administrator':
     return True
+  elif response['result']['status'] == 'left' or 'kicked':
+    kb = InlineKeyboardMarkup([[InlineKeyboardButton("اضغط للاشتراك ⏺", url="t.me/zx_xx")] ])
+    Bot("sendMessage",{"chat_id":chatID,"text":response["result"],"reply_to_message_id":message.message_id,"parse_mode":"html","disable_web_page_preview":True,"reply_markup":kb})
+    return False
